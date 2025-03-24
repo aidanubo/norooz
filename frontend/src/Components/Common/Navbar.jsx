d34d690a3d25128bc2b8e4a8c9546b0221528587
aidanubo <bagherianaida@gmail.com>
2025-03-24 15:05:14 +0330

    complete searchBar and create cartdrawer

--- frontend/src/Components/Common/Navbar.jsx
+++ frontend/src/Components/Common/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  HiOutlineUser,
+  HiOutlineShoppingBag,
+  HiBars3BottomRight,
+} from "react-icons/hi2";
+import SearchBar from "./SearchBar";
+import CartDrawer from "../Layout/CartDrawer";
 
 export default function Navbar() {
   return (
@@ -33,10 +40,29 @@ export default function Navbar() {
             to="#"
             className="text-gray-700 hover:text-black text-sm  font-medium uppercase"
           >
-            Bottom wear 
+            Bottom wear
           </Link>
         </div>
+        <div className="flex items-center space-x-4">
+          <Link to="/profile" className="hover:text-black">
+            <HiOutlineUser className="h-6 w-6 text-gray-700" />
+          </Link>
+          <button className="relative hover:text-black">
+            <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
+            <span className="absolute -top-1 bg-norooz-red text-white text-xs rounded-full px-2 py-0.5 ">
+              4
+            </span>
+          </button>
+          <div className="overflow-hidden">
+            <SearchBar />
+          </div>
+
+          <button className="md:hidden ">
+            <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
+          </button>
+        </div>
       </nav>
+      <CartDrawer/>
     </>
   );
 }
