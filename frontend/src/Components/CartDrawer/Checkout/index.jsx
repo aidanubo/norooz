f479102332fd538cdb1604066aa335baa19c5405
aidanubo <bagherianaida@gmail.com>
2025-04-03 17:31:35 +0330

    complete checkout

--- frontend/src/Components/CartDrawer/Checkout/index.jsx
+++ frontend/src/Components/CartDrawer/Checkout/index.jsx
@@ -1,9 +1,230 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const cart = {
+  products: [
+    {
+      name: "T-shirt",
+      size: "M",
+      color: "Black",
+      price: 120,
+      image: "https://picsum.photos/200?random=1",
+    },
+    {
+      name: "Jeans",
+      size: "42",
+      color: "White",
+      price: 75,
+      image: "https://picsum.photos/200?random=2",
+    },
+  ],
+  totalPrice: 195,
+};
 
 export default function Checkout() {
+  const navigate = useNavigate();
+  const [checkoutId, setCheckoutId] = useState(false);
+  const [shippingAddress, setShippingAddress] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleCreateCheckout = (e) => {
+    e.preventDefault();
+    setCheckoutId(120);
+  };
   return (
-    <div className=''>
-      
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
+      <div className="bg-white rounded-lg p-6">
+        <h2 className="text-2xl uppercase mb-6">Checkout</h2>
+        <form onSubmit={handleCreateCheckout}>
+          <h3 className="text-lg mb-4">Contact Details</h3>
+          <div className="mb-4">
+            <label className="block text-gray-700 ">Email</label>
+            <input
+              type="email"
+              value="user@example.com"
+              className="w-full p-2 rounded"
+              disabled
+            />
+          </div>
+          <h3 className="text-lg mb-4">Delivery</h3>
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700">First Name</label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    firstName: e.target.value,
+                  })
+                }
+                className="w-full p-2 rounded border"
+                value={shippingAddress.firstName}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700">Last Name</label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    lastName: e.target.value,
+                  })
+                }
+                className="w-full p-2 rounded border"
+                value={shippingAddress.lastName}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700">Address</label>
+            <input
+              type="text"
+              value={shippingAddress.address}
+              className="w-full p-2 border rounded"
+              required
+              onChange={(e) =>
+                setShippingAddress({
+                  ...shippingAddress,
+                  address: e.target.value,
+                })
+              }
+            />
+          </div>
+
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700">City</label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    city: e.target.value,
+                  })
+                }
+                className="w-full p-2 rounded border"
+                value={shippingAddress.city}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-gray-700">Postal Code</label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  setShippingAddress({
+                    ...shippingAddress,
+                    postalCode: e.target.value,
+                  })
+                }
+                className="w-full p-2 rounded border"
+                value={shippingAddress.postalCode}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700">Country</label>
+            <input
+              type="text"
+              value={shippingAddress.country}
+              className="w-full p-2 border rounded"
+              required
+              onChange={(e) =>
+                setShippingAddress({
+                  ...shippingAddress,
+                  country: e.target.value,
+                })
+              }
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700">Phone</label>
+            <input
+              type="text"
+              value={shippingAddress.phone}
+              className="w-full p-2 border rounded"
+              required
+              onChange={(e) =>
+                setShippingAddress({
+                  ...shippingAddress,
+                  phone: e.target.value,
+                })
+              }
+            />
+          </div>
+
+          <div className="mt-6">
+            {!checkoutId ? (
+              <button
+                type="submit"
+                className="w-full bg-black text-white py-3 rounded"
+              >
+                Continue To Payment
+              </button>
+            ) : (
+              <div>
+                <h3 className="text-lg mb-4">Pay To Paypal</h3>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+
+      <div className="bg-gray-50 p-6 rounded-lg">
+        <h3 className="text-lg mb-4">Order Summary</h3>
+        <div className="border-t py-4 mb-4">
+          {cart.products.map((product, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between py-4 border-b"
+            >
+              <div className="flex items-start">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-20 h-24 object-cover mr-4"
+                />
+                <div>
+                  <h3 className="text-md"> {product.name}</h3>
+                  <p className="text-gray-500">Size: {product.size}</p>
+                  <p className="text-gray-500">Color: {product.color}</p>
+                </div>
+              </div>
+              <p className="text-xl">${product.price?.toLocaleString()}</p>
+
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-between items-center text-ld mb-4">
+            <p>Subtotal</p>
+            <p>${cart.totalPrice?.toLocaleString()}</p>
+        </div>
+        <div className="flex justify-between items-center text-lg">
+            <p>Shipping</p>
+            <p>Free</p>
+        </div>
+        <div className="flex justify-between items-center text-lg mt-4 border-t pt-4">
+            <p>Total</p>
+            <p>${cart.totalPrice?.toLocaleString()}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
